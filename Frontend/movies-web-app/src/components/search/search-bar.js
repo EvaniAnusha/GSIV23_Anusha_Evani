576368d3467936b0c7d1fc5c4d4f3690067a905d
Anusha Evani <anusha.evani@fractal.ai>
2023-08-23 16:15:45 +0530

    search bar wip

--- Frontend/movies-web-app/src/components/search/search-bar.js
+++ Frontend/movies-web-app/src/components/search/search-bar.js
@@ -11,15 +11,43 @@ const { Search } = Input;
 
 const SearchBar = () => {
   const dispatch = useDispatch();
+  const { loadingList, searchResults } = useSelector((state) => state.listPage);
   const [searchValue, setSearchValue] = useState("");
+  const [searchPage, setSearchPage] = useState(1);
+  const handlingScroll = () => {
+    // const scrollThreshold = 200;
+    // const isScrollingUp = window.scrollY < scrollThreshold;
+    // if (isScrollingUp && searchPage > 1) {
+    //   setSearchPage((currPage) => currPage - 1);
+    //   return;
+    // } else
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      loadingList
+    ) {
+      return;
+    }
+    setSearchPage((prevPage) => prevPage + 1);
+  };
   const handleSearch = () => {
     dispatch(setSearchParam(searchValue));
-    dispatch(fetchSearchedMovies(searchValue));
+    dispatch(fetchSearchedMovies(searchValue, searchPage));
   };
   const handleOnClear = () => {
     dispatch(setSearchParam(""));
     dispatch(setSearchResults([]));
+    setSearchPage(1);
   };
+  useEffect(() => {
+    dispatch(fetchSearchedMovies(searchValue, searchPage));
+  }, [searchPage]);
+  useEffect(() => {
+    if (searchValue.length !== 0) {
+      window.addEventListener("scroll", handlingScroll);
+      return () => window.removeEventListener("scroll", handlingScroll);
+    }
+  }, [handlingScroll]);
   return (
     <Search
       className="search-bar"
